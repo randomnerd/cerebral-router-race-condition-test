3d2efc8d7a07f55802cd875903e3044abe09c2b2
Random Nerd <randomnerd@sickoffice.net>
2016-05-11 21:35:42 +0300

    logging

--- client/modules/Pages/index.js
+++ client/modules/Pages/index.js
@@ -5,7 +5,7 @@ import {Pages} from '/both/collections';
 function showPage ({input, state}) {
   state.set(['pages', 'permalink'], input.permalink);
   let page = Pages.findOne({permalink: input.permalink});
-  if (!page) return;
+  if (!page) { console.error('PAGE NOT FOUND'); return; }
   state.set(['pages', 'page'], page);
   Meteor.subs.subscribe('comments', page._id);
 }
